2116bc9e72378381a8367af70f044a0934486c44
TravisTroiani <travis.troiani@gmail.com>
2024-05-02 12:46:10 -0400

    removed waste

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -39,7 +39,7 @@ function createTaskCard(task) {
   const card = $("<div>")
     .addClass("card w-75 task-card draggable my-3")
     .attr("data-task-id", task.id);
-  // const backgroundColor = getRandomColor(); // Get random background color for text
+ 
   const title = $("<h3>").text(task.title);
 
   const description = $("<p>").text(task.description);
@@ -58,7 +58,6 @@ function createTaskCard(task) {
   return card;
 }
 
-// Create a function to render the task list and make cards draggable
 function renderTaskList() {
   const todoLane = $("#todo-cards");
   todoLane.empty();
@@ -68,7 +67,7 @@ function renderTaskList() {
 
   const doneLane = $("#done-cards");
   doneLane.empty();
-  // create variable for done lane
+  
 
   for (let task of taskList) {
     if (task.status === "to-do") {
@@ -80,17 +79,13 @@ function renderTaskList() {
   }
 
 
-  // ? Use JQuery UI to make task cards draggable
   $(".draggable").draggable({
     opacity: 0.7,
     zIndex: 100,
-    // ? This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
     helper: function (e) {
-      // ? Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
       const original = $(e.target).hasClass("ui-draggable")
         ? $(e.target)
         : $(e.target).closest(".ui-draggable");
-      // ? Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
       return original.clone().css({
         width: original.outerWidth(),
       });
@@ -98,7 +93,6 @@ function renderTaskList() {
   });
 }
 
-// Create a function to handle adding a new task
 function handleAddTask(event) {
   event.preventDefault();
 
@@ -119,14 +113,12 @@ function handleAddTask(event) {
     localStorage.setItem("tasks", JSON.stringify(taskList));
 
     renderTaskList();
-    // Clear input fields
     $("#task-title").val("");
     $("#task-description").val("");
     $("#task-deadline").val("");
   }
 }
 
-// // Create a function to handle deleting a task
 function handleDeleteTask(event) {
   event.preventDefault();
   const taskId = $(this).attr("data-task-id");
@@ -139,35 +131,28 @@ function handleDeleteTask(event) {
 
 // Create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-  // ? Get the project id from the event
   const taskId = ui.draggable[0].dataset.taskId;
 
-  // ? Get the id of the lane that the card was dropped into
   const newStatus = event.target.id;
 
   for (let task of taskList) {
-    // ? Find the project card by the `id` and update the project status.
     if (task.id === parseInt(taskId)) {
       task.status = newStatus;
     }
   }
-  // ? Save the updated projects array to localStorage (overwritting the previous one) and render the new project data to the screen.
   localStorage.setItem("tasks", JSON.stringify(taskList));
   renderTaskList();
 }
 
-// When the page loads, render the task list, add event listeners, make lanes droppable, and enable datepicker
 $(document).ready(function () {
   renderTaskList();
 
   $("#add-task-form").on("submit", handleAddTask);
 
-  // Make all task lanes droppable
   $(".lane").droppable({
     accept: ".draggable",
     drop: handleDrop,
   });
 });
 
-// // Enable datepicker on the deadline input field outside the document ready function
 $("#task-deadline").datepicker();
